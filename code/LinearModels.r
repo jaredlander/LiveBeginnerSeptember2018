d7e4e3659462e34c43f620328416d432c63b7086
Jared Lander <info@jaredlander.com>
2018-09-07 11:59:57 -0400

    Showed multiple regression

--- code/LinearModels.r
+++ code/LinearModels.r
@@ -33,3 +33,21 @@ build.x( ~ Pop + Size + Random, data=boros)
 build.x( ~ Boro, data=boros)
 
 build.x( ~ Pop + Size + Boro, data=boros)
+
+library(readr)
+
+land <- read_csv('data/manhattan_Train.csv')
+
+View(land)
+
+value1 <- lm(TotalValue ~ LotArea, data=land)
+value1
+
+value2 <- lm(TotalValue ~ LotArea + UnitsTotal, data=land)
+value2
+
+summary(value2)
+
+library(coefplot)
+coefplot(value2, sort='magnitude')
+coefplot(value2, sort='magnitude', intercept=FALSE)
