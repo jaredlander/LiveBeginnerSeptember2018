fbaac09bbfff996c7c169b282ee843f3f09f4c45
Jared Lander <info@jaredlander.com>
2018-09-07 12:55:51 -0400

    Predicted with GLMs

--- code/GeneralizedLinearModels.r
+++ code/GeneralizedLinearModels.r
@@ -18,3 +18,12 @@ coefplot(historic1, sort='magnitude')
 coefplot(historic1, sort='magnitude', intercept=FALSE)
 coefplot(historic1, sort='magnitude', intercept=FALSE, trans=invlogit)
 
+land_test <- readRDS('data/manhattan_Test.rds')
+set.seed(1234)
+land_test_small <- land_test %>% sample_n(20)
+land_test_small
+
+histPreds1 <- predict(historic1, newdata=land_test_small,
+                      se.fit=TRUE,
+                      type='response')
+histPreds1$fit
